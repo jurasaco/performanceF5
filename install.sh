4ae11d127f329048c71c23af5317bb4c6961e574
root <jurasaco@gmail.com>
2023-03-29 22:55:30 -0300

    version 2.2.1

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo "Updating from git"
 git pull
+echo "Installing requirements..."
+pip install pyinstaller
 pip install -r requirements.txt
+echo "Generating pyinstaller executable package..."
 pyinstaller --add-data 'modules/__report__.html:modules/' --onefile performanceF5.py
+echo "Copying performanceF5 executable to /usr/bin/..."
 cp -f dist/performanceF5 /usr/bin/
+echo "Done!"
