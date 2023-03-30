f1e5472ed0f96c493ec78737bad210f2ee710466
root <jurasaco@gmail.com>
2023-03-29 22:49:47 -0300

    version 2.2.1

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
-pip install requirements.txt
+pip install -r requirements.txt
 pyinstaller --add-data 'modules/__report__.html:modules/' --onefile performanceF5.py
 cp -f dist/performanceF5 /usr/bin/
